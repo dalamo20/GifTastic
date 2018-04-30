5af6e5276ad48171bbd2c45220d2bd843cc2370f
Dalamo <dalamo90@gmail.com>
2018-04-29 23:36:49 -0500

    img's aligned

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,12 +2,13 @@ $(document).ready(function () {
     var topics = ["coffee", "dogs", "cats", "nerds"]
 
     var API_KEY = "LZIwckN56pSuLgxseZy0SZbHTjDPEfzg";
-    var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&limit=10&q=";
+    var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&rating&limit=10&q=";
 
     for (var i = 0; i < topics.length; i++) {
         var button = $("<button>");
         button.addClass("btn btn-info");
         button.text(topics[i]);
+        //create one event handler for all buttons: BONUS!
         button.on("click", function () {
             $("#gifs").empty();
             $.ajax({
@@ -16,13 +17,11 @@ $(document).ready(function () {
             }).then(function (response) {
                 console.log(response.data);
                 for (var i = 0; i < response.data.length; i++) {
-                    var img = $("<img>");
-                    // set the src of the element
-                    img.attr("src", response.data[i].images.downsized.url);
-                    img.attr("data-animated", response.data[i].images.downsized.url);
-                    img.attr("data-still", response.data[i].images.downsized_still.url);
-                    img.attr("data-state", "animated");
+                    var img = createImage(response);
+                    // img.attr("data-rating"), response.data[i].rating);
 
+                    //BONUS: Try to make an event handlerfor all img's
+                    //FIX: Images should start still    
                     img.on("click", function () {
                         var state = $(this).attr("data-state");//creating variable state and asigning it the value of data-state which is animated
                         console.log(state); // should output animated as a value
@@ -36,9 +35,9 @@ $(document).ready(function () {
                         } else {
 
                             //else change state to animated
-                            $(this).attr(state, "animated");
+                            $(this).attr("data-state", "animated");
                             //change src to still version
-                            $(this).attr("src", "data-animated");
+                            $(this).attr("src", $(this).attr("data-animated"));
                         }
                     });
 
@@ -48,4 +47,14 @@ $(document).ready(function () {
         });
         $("#buttons").append(button);
     }
+    function createImage(response) {
+        var img = $("<img>");
+        // set the src of the element
+        img.attr("src", response.data[i].images.downsized.url);
+        img.attr("data-animated", response.data[i].images.downsized.url);
+        img.attr("data-still", response.data[i].images.downsized_still.url);
+        img.attr("data-state", "animated");
+
+        return img;
+    };
 });
